d551f62e5c9e6308161d9cc8b85e3e2d5260b7ad
juicyjung <travelandi01@gmail.com>
2023-06-30 17:59:32 +0900

    modify : outside code

--- test_outside.py
+++ test_outside.py
@@ -1,5 +1,7 @@
 from model import Model
 
-model = Model()     # Class 선언
+# Model Class 선언
+model = Model()
 
-model.request("model/data/Loan payments data.xlsx", '이 사람들의 학력을 그래프로 만들어줘')
+# data랑 prompt 넣고 함수 호출
+output = model.request("model/data/Loan payments data.xlsx", '이 사람들의 학력을 그래프로 만들어줘')
